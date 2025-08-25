a4808278a82a52919066d91cb7e9aff9ee761277
fastman <15538091226@163.com>
2025-08-25 16:36:21 +0800

    Add files via upload

---
+++ runtime/temp/d608fcb68b9c6b7ff1d6780c21a3da80.php
@@ -0,0 +1,16 @@
+<?php /*a:1:{s:69:"/www/wwwroot/app.84000lookingfor.com/niucloud/public/admin/index.html";i:1748821820;}*/ ?>
+<!DOCTYPE html>
+<html lang="zh-cn">
+  <head>
+    <meta charset="UTF-8" />
+    <link rel="icon" type="image" href="/admin/niucloud.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title></title>
+    <script type="module" crossorigin src="/admin/assets/index-9a6391c2.js"></script>
+    <link rel="stylesheet" href="/admin/assets/index-3092ffa4.css">
+  </head>
+  <body>
+    <div id="app"></div>
+    
+  </body>
+</html>
